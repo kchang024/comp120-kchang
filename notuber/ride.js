60c18e9d82d74f2da90e05c4a681acff0e283d51
Kevin Chang <kchang024@gmail.com>
2020-06-09 19:43:03 -0400

    basic reqs finished, need to optimize performance, add features/explore

--- notuber/ride.js
+++ notuber/ride.js
@@ -12,10 +12,49 @@ function initMap() {
         });
 
   // Marker for South Station
-  var marker = new google.maps.Marker({
+  var centerMarker = new google.maps.Marker({
     position: southStation, 
     map: map
   });
+
+  // Car icon for display in Google Maps
+  var icons = {
+    car: {
+      name: 'Car',
+      icon: 'car.png'
+    }
+  };
+
+  var carLocations = [
+  { 
+    position: new google.maps.LatLng(42.3453, -71.0464),
+    type: 'car'
+  }, {
+    position: new google.maps.LatLng(42.3662, -71.0621),
+    type: 'car'
+  }, {
+    position: new google.maps.LatLng(42.3603, -71.0547),
+    type: 'car'
+  }, {
+    position: new google.maps.LatLng(42.3472, -71.0802),
+    type: 'car'
+  }, {
+    position: new google.maps.LatLng(42.3663, -71.0544),
+    type: 'car'
+  }, {
+    position: new google.maps.LatLng(42.3542, -71.0704),
+    type: 'car'
+  }
+  ];
+
+  carLocations.forEach(function(carLocations) {
+    var marker = new google.maps.Marker ( {
+      position: carLocations.position,
+      icon : icons[carLocations.type].icon,
+      map: map
+    });
+  });
+
 }
    
 
