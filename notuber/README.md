0460c6f80192d75e6269acd983480da695ac94f5
Kevin Chang <kchang024@gmail.com>
2020-06-17 20:03:11 -0400

    final submit, all completed, 1 bonus done

--- notuber/README.md
+++ notuber/README.md
@@ -3,18 +3,22 @@
 
     Assignment    : Lab 10 - The Ride-Hailing Service, Part 2
     Author        : Kevin Chang
-    Date          : 
+    Date          : 06-17-20
 
     Correct Implementation/ Incorrect Implementation:
     -------------------------------------------------
-	
+	I believe all the requirements and things on the spec have been done
+    correctly.
+
+    I was concerned that on the browser's console it showed that I have
+    3 violations?
 
     Acknowledgements/collaborated:
     ------------------------------
-    
+    N/A
 	        
     Hours spent completing the lab:
     ------------------------------
-	
+	~ 6 hours
 
     
--- notuber/ride.js
+++ notuber/ride.js
@@ -2,9 +2,10 @@ var map;
 var myLat = 0;
 var myLng = 0;
 
+
+// initialization function
 // This function is called via the script src in the html
 function initMap() {
-  //Location of initial map
   var myLocation = new google.maps.LatLng(myLat, myLng);
 
   // The map, centered on retrieved geolocation
@@ -16,12 +17,15 @@ function initMap() {
   getLocation();
 }
 
+// gets the location of the user based on google's geolocation
+// if location is found app countinues to look for car rides
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(somePos) {
       myLat = somePos.coords.latitude;
       myLng = somePos.coords.longitude;
       makeMap();
+      hailRides();
     }); 
   }
   else {
@@ -29,67 +33,60 @@ function getLocation() {
   }
 }
 
-
+// makes the map depending on the user's geolocation
 function makeMap() {
   myLocation = new google.maps.LatLng(myLat, myLng);
-
   map.panTo(myLocation);
- 
-  console.log("hi from makemap");
-
-  hailRides();
 }
 
-
+// Uses the ride hailing API to get a list of cars "nearby" the user
 function hailRides() {
-
-  console.log("hi from hail");
-
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "https://jordan-marsh.herokuapp.com/rides", true);
   xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-  // CODE FOR ONREADY HERE
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       // get data 
       var responseData = xhr.responseText;
-
       // parse data into list
       var parsedData = JSON.parse(responseData);
-
       // to set the boundary of the map according to the markers
       var bounds = new google.maps.LatLngBounds();
-
       // initial declare for variable to hold closest distance
       var distClosest = 1000000.11;
 
-      // marks each available ride on the map
+      var carInfo = new google.maps.InfoWindow();
+
+      // loops through the returned string from the ride-hailing API
+      // sets markers for each available ride on the map
       // extending the bounds of the map as needed
       for (var i = 0; i < parsedData.length; i++){
         rideLat = parsedData[i]['lat'];
         rideLng = parsedData[i]['lng'];
         rideLocation = new google.maps.LatLng(rideLat, rideLng);
 
-        console.log(rideLocation);
-
+        // makes a marker for each service car
         var marker = new google.maps.Marker({
           position: rideLocation,
           icon: 'car.png',
-          map: map
+          map: map,
+          title: "notUber car"
         });
 
         // calculates distance between your location and service car
         distBetw = google.maps.geometry.spherical.computeDistanceBetween(myLocation, rideLocation);
-        console.log("diff in meters: " + distBetw);
-        console.log("closest: " + distClosest);
+        distBetwMiles = toMiles(distBetw);
 
-        if (distBetw < distClosest) {
-          distClosest = distBetw;
-          console.log("in if");
+        // makes a info window for each service car and displays ETA to user
+        makeInfoWindow(marker, map, carInfo, "This car is " + distBetwMiles + " miles away from you");
+
+        // gets and sets info on the closest service car in meters 
+        if (distBetwMiles < distClosest) {
+          distClosest     = distBetwMiles;
           var closestUser = parsedData[i]['username'];
-          var closestLat = rideLat;
-          var closestLng = rideLng;
+          var closestLat  = rideLat;
+          var closestLng  = rideLng;
         }
 
         bounds.extend(rideLocation);
@@ -97,11 +94,8 @@ function hailRides() {
 
       map.fitBounds(bounds);
 
-      // converts the distance from meters to miles
-      distClosest = distClosest * 0.000621371;
-
       // Marker for myLocation
-      var centerMarker = new google.maps.Marker({
+      var yourMarker = new google.maps.Marker({
         position: myLocation, 
         map: map,
         title: "Hi Ming! Based on Google's Geolocation, this is where YOU are!"
@@ -112,45 +106,48 @@ function hailRides() {
 
       var ContentString = '<h1>Where is my notUber?</h1>' +
                           '<div id="bodyContent">' +
-                          '<p>Closest vehicle is: '  + closestUser + '</p>' + 
+                          '<p>Closest car is: '  + closestUser + '</p>' + 
                           '<p>Currently at: ' + closestLat + ", " + closestLng  + '</p>' +
                           '<p>' + closestUser + ' is ' + distClosest + ' miles away' +'</p>'
                           '</div>';
 
-      google.maps.event.addListener(centerMarker, 'click', function() {
-        infowindow.setContent(ContentString);
-        infowindow.open(map, centerMarker);
-      });
+      makeInfoWindow(yourMarker, map, infowindow, ContentString);
 
-      //adding polyline paths
-      var linePath = [
-        {lat: myLat, lng: myLng},
-        {lat: closestLat, lng: closestLng}
-      ];
+      drawPolyline(myLat, myLng, closestLat, closestLng);
+    }
+    else if (xhr.readyState == 4 && xhr.status != 200) {
+      document.getElementById("map").innerHTML = request.status + "<p> ERROR </p>";
+    }
+  };
 
+  xhr.send("username=RgSVguNn&lat=" + myLat + "&lng=" + myLng);
+}
+
+// draws a red polyline showing a path between two locations
+function drawPolyline (myLat, myLng, closestLat, closestLng) {
       var polyLine = new google.maps.Polyline({
-        path: linePath,
+        path: [
+          {lat: myLat, lng: myLng},
+          {lat: closestLat, lng: closestLng}
+        ],
         geodesic: true,
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
         strokeWeight: 2
       });
+
       polyLine.setMap(map);
-
-      console.log("final closest: " + distClosest);
-      console.log(parsedData);
-      console.log("hi from onreadystatechange, NOW READY");
-    }
-    else if (xhr.readyState == 4 && xhr.status != 200) {
-      console.log("ERROR, something isn't right");
-    }
-    else {
-      console.log("not complete, in progress");
-    }
-  };
-
-  // CODE TO SEND HERE
-  xhr.send("username=RgSVguNn&lat=" + myLat + "&lng=" + myLng);
 }
 
+// converts meters to miles
+function toMiles (meters) {
+  return meters * 0.000621371;
+}
 
+// creates an google InfoWindow and sets its contents
+function makeInfoWindow (marker, map, carInfo, content) {
+  marker.addListener('click', function() {
+    carInfo.setContent(content);
+    carInfo.open(map, this);
+  }, {passive: true});
+}
